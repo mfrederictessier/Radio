a3fcfcc7f973240864389b9b1a62485873e9089a
mfrederictessier <162064632+mfrederictessier@users.noreply.github.com>
2024-06-17 16:05:06 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -50,7 +50,18 @@ const stationsSaguenay = [
 ];
 const stationsMontreal = [
 
-    
+    {
+        tuning: "94,3",
+        name: "Energie Montréal (CKMF-FM)",
+        url: " https://playerservices.streamtheworld.com/api/livestream-redirect/CKMFFMAAC_SC.aac",
+        logo: "https://www.radioenergie.ca/content/dam/audio/iheart-stations-logos/logo-nrj-montreal.svg"
+    },
+    {
+        tuning: "96,9",
+        name: "CKOI Montréal (CKOI-FM)",
+        url: " https://playerservices.streamtheworld.com/api/livestream-redirect/CKOIFM_SC.aac",
+        logo: "https://cdn.cogecolive.com/websites-public/logo_ckoi_color_default.svg"
+    },
     {
         tuning: "97,7",
         name: "CHOM 97.7 Montréal",
@@ -65,13 +76,19 @@ const stationsMontreal = [
     },
     {
         tuning: "99,5",
-        name: "Radio Classique",
+        name: "Radio Classique Montréal",
         url: " https://listen.radioking.com/radio/228241/stream/271810",
         logo: "https://cdn.onlineradiobox.com/img/l/5/10905.v10.png"
     },
     {
+        tuning: "100,7",
+        name: "ICI Musique - Montréal",
+        url: "  https://playerservices.streamtheworld.com/api/livestream-redirect/CBFXFM_SRC.mp3",
+        logo: "https://th.bing.com/th/id/OIP.lRfpQPAqQD76xGEvOGQysAAAAA?w=118&h=50&c=7&r=0&o=5&pid=1.7"
+    },
+    {
         tuning: "105,7",
-        name: "Rythme FM",
+        name: "Rythme FM Montréal",
         url: " https://playerservices.streamtheworld.com/api/livestream-redirect/CFGLFM.mp3",
         logo: "https://cdn.onlineradiobox.com/img/l/0/16960.v12.png"
     },
@@ -84,6 +101,24 @@ const stationsMontreal = [
 ];
 const stationsQuebec = [
 
+    {
+        tuning: "92,7",
+        name: "Radio-Classique - Québec (CJSQ-FM)",
+        url: " https://www.radioking.com/play/radio-classique-quebec-92-7",
+        logo: "https://www.radioclassique.ca/upload/players/5e3c736c225953.40751304.png"
+    },
+    {
+        tuning: "95,3",
+        name: "ICI Musique - Québec",
+        url: " https://playerservices.streamtheworld.com/api/livestream-redirect/CBVXFM_SRC.mp3",
+        logo: "https://cdn-profiles.tunein.com/s12490/images/logod.png?t=637552187150000000"
+    },
+    {
+        tuning: "98,1",
+        name: "Radio X - Québec (CHOI-FM)",
+        url: " https://lb0-stream.radiox981.ca/choi.mp3",
+        logo: "https://media.radiox.com/2023/04/choi-logo-mc-fond-pale.svg"
+    },
     {
         tuning: "98,9",
         name: "Energie Québec (CHIK-FM)",
@@ -112,6 +147,61 @@ const stationsAlma = [
 
     
 ];
+const stationsMauricie = [
+
+    {
+        tuning: "94,7",
+        name: "Rouge FM Mauricie (CHEY-FM)",
+        url: "https://playerservices.streamtheworld.com/api/livestream-redirect/CHEYFM_ADP.aac",
+        logo: "https://www.rougefm.ca/content/dam/audio/iheart-stations-logos/logo-rougefm-mauricie.svg"
+    },
+    {
+        tuning: "96,5",
+        name: "ICI Radio-Canada Première Mauricie - Centre-du-Québec",
+        url: " https://playerservices.streamtheworld.com/api/livestream-redirect/CBFFM8_SRC.mp3",
+        logo: "https://th.bing.com/th/id/OIP.yPjxwW0z-vxjF6KX4odZNQHaB5?w=336&h=89&c=7&r=0&o=5&pid=1.7"
+    },
+    {
+        tuning: "98,1",
+        name: "Energie Mauricie (Trois-Rivières) (CIGB-FM)",
+        url: "https://playerservices.streamtheworld.com/api/livestream-redirect/CIGBFM_ADP.aac",
+        logo: "https://www.radioenergie.ca/content/dam/audio/iheart-stations-logos/logo-nrj-mauricie.svg"
+    },
+    {
+        tuning: "100,1",
+        name: "Rythme FM Mauricie (CJEB-FM)",
+        url: "https://playerservices.streamtheworld.com/api/livestream-redirect/CJEBFM_SC.aac",
+        logo: "https://cdn.cogecolive.com/websites-public/logo_rythme_mauricie_color_ripple.svg"
+    },
+    {
+        tuning: "104,3",
+        name: "ICI Musique - Mauricie/Centre-du-Québec",
+        url: "http://playerservices.streamtheworld.com/api/livestream-redirect/CBFXFM1_SRC.mp3",
+        logo: "https://th.bing.com/th/id/OIP.BLuaK5ii4mwKKJuGyTKh0gHaGW?w=185&h=180&c=7&r=0&o=5&pid=1.7"
+    },
+];
+const stationsOttawa = [
+    {
+        tuning: "91,5",
+        name: "CBC Radio One - Ottawa",
+        url: "https://playerservices.streamtheworld.com/api/livestream-redirect/CBOFM_CBC.mp3",
+        logo: "https://cdn.onlineradiobox.com/img/l/1/11431.v10.png"
+    },
+    {
+        tuning: "95,1",
+        name: "ICI Radio-Canada Première Ottawa/Gatineau",
+        url: "https://playerservices.streamtheworld.com/api/livestream-redirect/CBOFFM_SRC.mp3",
+        logo: "https://th.bing.com/th/id/OIP.xZ_FmjDL6mA5mecG5yMyRwHaFf?w=236&h=180&c=7&r=0&o=5&pid=1.7"
+    },
+    {
+        tuning: "102,5",
+        name: "ICI Musique - Ottawa/Gatineau",
+        url: " https://playerservices.streamtheworld.com/api/livestream-redirect/CBOXFM_SRC.mp3",
+        logo: "https://cdn.onlineradiobox.com/img/l/1/19341.v7.png"
+    },
+
+    
+];
 const stationList = document.getElementById('stationList');
 const frequencyTicks = document.getElementById('frequencyTicks');
 const audioPlayer = document.getElementById('audioPlayer');
@@ -130,6 +220,10 @@ function getStationsForRegion(regionName) {
             return stationsQuebec;
         case 'Sherbrooke':
             return stationsSherbrooke;
+        case 'Mauricie':
+            return stationsMauricie;
+        case 'Ottawa':
+            return stationsOttawa;
         // Add other cases for other regions
         default:
             return [];
@@ -232,7 +326,7 @@ var map = L.map('map', {
     center: [48, -70],
     zoom: 5,
     minZoom: 3,  // Définir le zoom minimum
-    maxZoom: 10, // Définir le zoom maximum
+    maxZoom: 5, // Définir le zoom maximum
     maxBounds: [
         [-85, -179.5], // Sud-Ouest
         [85, 179.5]    // Nord-Est
@@ -264,72 +358,119 @@ var regions = [
     {name: "Quebec", coords: [46.8139, -71.2082]},
     {name: "Montreal", coords: [45.5017, -73.5673]},
     {name: "Sherbrooke", coords: [45.4042, -71.8929]},
-    // {name: "Saint-Hyacinthe", coords: [45.6308, -72.9567]},
-    // {name: "Saint-Jean-sur-Richelieu", coords: [45.3070, -73.2623]},
-    // {name: "Drummondville", coords: [45.8833, -72.4854]},
-    // {name: "Est-du-Quebec", coords: [48.3581, -68.5811]},
-    // {name: "Estrie", coords: [45.4103, -71.8820]},
-    // {name: "Gatineau-Ottawa", coords: [45.4765, -75.7013]},
-    // {name: "Mauricie", coords: [46.6090, -72.2081]},
-    // {name: "Rouyn-Noranda", coords: [48.2324, -79.0283]},
-    // {name: "Val-d'Or", coords: [48.0972, -77.7974]},
-    // {name: "Amqui", coords: [48.4634, -67.4349]},
-    // {name: "Rimouski", coords: [48.4333, -68.5230]},
-    // {name: "Alma", coords: [48.5500, -71.6500]},
-    // {name: "Chibougamau", coords: [49.9133, -74.3683]},
-    // {name: "Dolbeau-Mistassini", coords: [48.8836, -72.2256]},
-    // {name: "Roberval", coords: [48.5209, -72.2227]},
-    // {name: "Trois-Rivieres", coords: [46.3430, -72.5467]},
-    // {name: "Saint-Jerome", coords: [45.7807, -74.0036]},
-    // {name: "Mont-Tremblant", coords: [46.1184, -74.5962]},
-    // {name: "Lachute", coords: [45.6526, -74.3252]},
-    // {name: "Hawkesbury", coords: [45.6070, -74.6044]},
-    // {name: "La-Pocatiere", coords: [47.3641, -70.0347]},
-    // {name: "Riviere-du-Loup", coords: [47.8361, -69.5333]},
-    // {name: "Montmagny", coords: [46.9794, -70.5500]},
-    // {name: "Saint-Georges-de-Beauce", coords: [46.1214, -70.6690]},
-    // {name: "Baie-Comeau", coords: [49.2167, -68.1500]},
-    // {name: "Cote-Nord", coords: [50.0000, -66.6667]},
-    // {name: "Gaspesie-Iles-de-la-Madeleine", coords: [48.7743, -64.4810]},
-    // {name: "Grand-Nord", coords: [51.8918, -76.0513]},
-    // {name: "Ile-du-Prince-Edouard", coords: [46.2500, -63.0000]},
-    // {name: "Nord-de-l'Ontario", coords: [49.7592, -84.3631]},
-    // {name: "Ottawa", coords: [45.4215, -75.6972]},
-    // {name: "Toronto", coords: [43.651070, -79.347015]},
-    // {name: "Windsor", coords: [42.3149, -83.0364]},
-    // {name: "Manitoba", coords: [49.8951, -97.1384]},
-    // {name: "Saskatchewan", coords: [52.9399, -106.4509]},
-    // {name: "Nouveau-Brunswick", coords: [45.9636, -66.6431]},
-    // {name: "Nouvelle-Ecosse", coords: [44.6820, -63.7443]},
-    // {name: "Terre-Neuve-et-Labrador", coords: [53.1355, -57.6604]}
+    {name: "Saint-Hyacinthe", coords: [45.6308, -72.9567]},
+    {name: "Saint-Jean-sur-Richelieu", coords: [45.3070, -73.2623]},
+    {name: "Drummondville", coords: [45.8833, -72.4854]},
+    {name: "Est-du-Quebec", coords: [48.3581, -68.5811]},
+    {name: "Estrie", coords: [45.4103, -71.8820]},
+    {name: "Gatineau-Ottawa", coords: [45.4765, -75.7013]},
+    {name: "Mauricie", coords: [46.6090, -72.2081]},
+    {name: "Rouyn-Noranda", coords: [48.2324, -79.0283]},
+    {name: "Val-d'Or", coords: [48.0972, -77.7974]},
+    {name: "Amqui", coords: [48.4634, -67.4349]},
+    {name: "Rimouski", coords: [48.4333, -68.5230]},
+    {name: "Alma", coords: [48.5500, -71.6500]},
+    {name: "Chibougamau", coords: [49.9133, -74.3683]},
+    {name: "Dolbeau-Mistassini", coords: [48.8836, -72.2256]},
+    {name: "Roberval", coords: [48.5209, -72.2227]},
+    {name: "Trois-Rivieres", coords: [46.3430, -72.5467]},
+    {name: "Saint-Jerome", coords: [45.7807, -74.0036]},
+    {name: "Mont-Tremblant", coords: [46.1184, -74.5962]},
+    {name: "Lachute", coords: [45.6526, -74.3252]},
+    {name: "Hawkesbury", coords: [45.6070, -74.6044]},
+    {name: "La-Pocatiere", coords: [47.3641, -70.0347]},
+    {name: "Riviere-du-Loup", coords: [47.8361, -69.5333]},
+    {name: "Montmagny", coords: [46.9794, -70.5500]},
+    {name: "Saint-Georges-de-Beauce", coords: [46.1214, -70.6690]},
+    {name: "Baie-Comeau", coords: [49.2167, -68.1500]},
+    {name: "Cote-Nord", coords: [50.0000, -66.6667]},
+    {name: "Gaspesie-Iles-de-la-Madeleine", coords: [48.7743, -64.4810]},
+    {name: "Grand-Nord", coords: [51.8918, -76.0513]},
+    {name: "Ile-du-Prince-Edouard", coords: [46.2500, -63.0000]},
+    {name: "Nord-de-l'Ontario", coords: [49.7592, -84.3631]},
+    {name: "Ottawa", coords: [45.4215, -75.6972]},
+    {name: "Toronto", coords: [43.651070, -79.347015]},
+    {name: "Windsor", coords: [42.3149, -83.0364]},
+    {name: "Manitoba", coords: [49.8951, -97.1384]},
+    {name: "Saskatchewan", coords: [52.9399, -106.4509]},
+    {name: "Nouveau-Brunswick", coords: [45.9636, -66.6431]},
+    {name: "Nouvelle-Ecosse", coords: [44.6820, -63.7443]},
+    {name: "Terre-Neuve-et-Labrador", coords: [53.1355, -57.6604]}
 ];
 
 // Boucle forEach pour ajouter les marqueurs sur la carte
 regions.forEach(function(region) {
     var stations = getStationsForRegion(region.name); // Récupérer les stations pour la région actuelle
-    var popupContent = `<strong>${region.name}</strong><br>`;
-    popupContent += `Nombre de stations disponibles: ${stations.length}<br>`;
-    stations.forEach(station => {
-        popupContent += `${station.tuning} - ${station.name}<br>`;
-    });
-    var adjustedCoords = [region.coords[0] + 1, region.coords[1]];
-    let content = `<strong>${region.name}</strong><br>`;
-    content += `Nombre de stations disponibles: ${stations.length}<br>`;
-    // Ajouter un marqueur pour cette région
-    L.marker(region.coords).addTo(map)
-        .bindPopup(popupContent)
-        .on('click', function() {
+    if (stations.length > 0) {
+        var popupContent = `<strong>${region.name}</strong><br>`;
+        popupContent += `Nombre de stations disponibles: ${stations.length}<br>`;
+        stations.forEach(station => {
+            popupContent += `${station.tuning} - ${station.name}<br>`;
+        });
+        
+        // Calculer le rayon du cercle en fonction du nombre de stations
+        var radius = 50000; // Exemple : 1000 mètres par station
+        
+        // Créer le cercle de couverture
+        L.circle(region.coords, {
+            radius: radius,
+            weight: 1,
+            color: 'yellowgreen', // Couleur du contour du cercle
+            fillColor: 'yellowgreen', // Couleur de remplissage du cercle
+            fillOpacity: 0.3 // Opacité du remplissage (0 = transparent, 1 = opaque)
+        }).addTo(map);
+        // Créer le cercle de couverture
+        L.circle(region.coords, {
+            radius: 100000,
+            weight: 1,
+            color: 'red',
+            fillColor: 'red', // Couleur de remplissage du cercle
+            fillOpacity: 0.1 // Opacité du remplissage (0 = transparent, 1 = opaque)
+        }).addTo(map);
+        var adjustedCoords = [region.coords[0] + 1, region.coords[1]];
+        let content = `<strong>${region.name}</strong><br>`;
+        content += `Nombre de stations disponibles: ${stations.length}<br>`;
+        // Ajouter un marqueur pour cette région
+        var popupOpen = false; // Variable pour suivre l'état du popup
+
+        // Créer le marqueur avec popup
+        var marker = L.marker(region.coords).addTo(map)
+            .bindPopup(popupContent);
+
+        // Gestion des événements
+        marker.on('click', function() {
             // Mettre à jour la valeur du sélecteur de région
             document.getElementById('regionSelector').value = region.name;
             // Charger les stations pour la région sélectionnée
             loadStations(region.name);
-            
-        })
-        .on('mouseover', function() {
-            L.popup()
-                .setLatLng(adjustedCoords)
-                .setContent(content)
-                .openOn(map);});
+            popupOpen = true;
+        });
+
+        marker.on('mouseout', function() {
+            // Fermer le popup lorsque la souris quitte le marqueur
+            if (!popupOpen) {
+                map.closePopup();
+            }
+        });
+        marker.on('mouseover', function() {
+            // Ouvrir le popup au survol si le popup n'est pas déjà ouvert
+            if (!popupOpen) {
+                L.popup()
+                    .setLatLng(adjustedCoords)
+                    .setContent(content)
+                    .openOn(map);
+            }
+        });
+
+        marker.on('popupopen', function() {
+            popupOpen = true; // Mettre à jour l'état du popup à ouvert
+        });
+
+        marker.on('popupclose', function() {
+            popupOpen = false; // Mettre à jour l'état du popup à fermé
+        });
+
+    }
 });
 
 
@@ -343,10 +484,10 @@ document.getElementById('regionSelector').addEventListener('change', function() 
             stations.forEach(station => {
                 popupContent += `${station.tuning} - ${station.name}<br>`;
             });
-        map.setView(region.coords, 7);
-
+        map.setView(region.coords, 5);
+        var adjustedCoords = [region.coords[0] + 1, region.coords[1]];    
         L.popup()
-        .setLatLng(region.coords)
+        .setLatLng(adjustedCoords)
         .setContent(popupContent)
         .openOn(map);
 
@@ -398,18 +539,21 @@ toggleControlsButton.addEventListener('click', () => {
 
 playButton.addEventListener('click', () => {
     audioPlayer.play();
+    document.querySelector('.controls').classList.remove('pause', 'stop');
     document.querySelector('.controls').classList.add('playing');
 });
 
 pauseButton.addEventListener('click', () => {
     audioPlayer.pause();
-    document.querySelector('.controls').classList.remove('playing');
+    document.querySelector('.controls').classList.remove('playing', 'stop');
+    document.querySelector('.controls').classList.add('pause');
 });
 
 stopButton.addEventListener('click', () => {
     audioPlayer.pause();
     audioPlayer.currentTime = 0;
-    document.querySelector('.controls').classList.remove('playing');
+    document.querySelector('.controls').classList.remove('playing', 'pause');
+    document.querySelector('.controls').classList.add('stop');
 });
 
 volumeSlider.addEventListener('input', (event) => {
@@ -456,3 +600,4 @@ audioPlayer.addEventListener('play', updateFrequencyDisplay);
 audioPlayer.addEventListener('pause', updateFrequencyDisplay);
 
 updateFrequencyDisplay();
+
